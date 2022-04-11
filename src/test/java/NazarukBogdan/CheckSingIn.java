f2c3132a623355289af53f2faab6073b9b619fd1
Bogdan27nazaruk <bodya.nazaruk409@gmail.com>
2022-04-11 14:41:15 +0300

    newSignInPR

---
+++ src/test/java/NazarukBogdan/CheckSingIn.java
@@ -0,0 +1,25 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CheckSingIn {
+    @Test
+    public void newSignIn() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://qa2.eatstreet.com/");
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("//a[@id='menu-signin']")).click();
+        driver.findElement(By.xpath("//a[contains(text(), 'Sign Up')]")).click();
+        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("BogNazzz@gmail.com");
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("1237894560qaz.N");
+        driver.findElement(By.xpath("//input[@id='passwordAgain']")).sendKeys("1237894560qaz.N");
+        driver.findElements(By.xpath("//div[@ng-if='displayPasswordToggler']")).get(1).click();
+        driver.findElement(By.xpath("//button[@id='signup']")).click();
+        Assert.assertTrue(driver.getCurrentUrl().contains("com"));
+        driver.quit();
+    }
+}
