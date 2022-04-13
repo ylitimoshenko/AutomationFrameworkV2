4a82ef39f8732129063f5bc8cad0d6068ec2861d
Bogdan27nazaruk <bodya.nazaruk409@gmail.com>
2022-04-13 18:52:29 +0300

    FixedSearchRestaurantsInNewYork

--- src/test/java/NazarukBogdan/SearchRestaurantsinNewYork.java
+++ src/test/java/NazarukBogdan/SearchRestaurantsinNewYork.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SearchRestaurantsinNewYork {
@@ -26,10 +27,8 @@ public class SearchRestaurantsinNewYork {
         driver.findElement(By.xpath("//a[@id='find-restaurants']")).click();
         sleep(2);
         driver.findElement(By.xpath("//input[@id='filters-checkbox-takeout']")).click();
+        sleep(2);
+        Assert.assertTrue(driver.findElement(By.xpath("//h1[contains(text(), 'New York City Restaurants That Deliver & Takeout')]")).isDisplayed());
         driver.quit();
-
-
     }
-
-
 }
