7b0593d39f110de0d2f1ecbbca802ad31e2bec65
Nestors <nestors@Yuliya-Kulyk-MacBook-Pro.local>
2022-04-14 18:51:55 +0300

    latest webdriver version is added

--- src/test/java/pageobject/BasePage.java
+++ src/test/java/pageobject/BasePage.java
@@ -6,19 +6,21 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public abstract class BasePage {
 
     int BASIC_TIME = 15;
 
     public List<WebElement> waitVisibilityOfAllElementsLocatedBy(String locator) {
-        WebDriverWait wait = new WebDriverWait(driver, BASIC_TIME);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         return wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(locator)));
     }
 
     public WebElement waitElementToBeClickable(String locator) {
-        WebDriverWait wait = new WebDriverWait(driver, BASIC_TIME);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         return wait.until(ExpectedConditions.elementToBeClickable(By.xpath(locator)));
     }
     public static WebDriver driver;
