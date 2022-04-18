ae6fd409435faafe1c97e3ca3f70e6564ec882eb
Nestors <nestors@Yuliya-Kulyk-MacBook-Pro.local>
2022-04-18 14:26:41 +0300

    fixed Homepage

--- src/test/java/KulykYuliya/Pages/HomePage.java
+++ src/test/java/KulykYuliya/Pages/HomePage.java
@@ -4,32 +4,4 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import pageobject.BasePage;
 
-public class HomePage extends BasePage {
-    public HomePage(WebDriver driver) {
-        super(driver);
-    }
 
-    public void OpenSiteEatStreet() {
-        driver.get("https://qa2.eatstreet.com/");
-    }
-
-    public void enterAddress() {
-        findElementByXpath("//input[@id='input-food-search']").sendKeys("5000 South Arizona Mills Circle");
-
-    }
-
-    public WebElement clichFed() {
-        return findElementByXpath("//a[@id='find-restaurants']");
-    }
-
-
-    public WebElement clickEnterAddress() {
-        return findElementByXpath("//a[@id='enter-address-btn']");
-
-    }
-
-
-    public WebElement arizona() {
-        return findElementByXpath("//li[contains(text(), 'Arizona Mills, South Arizona Mills Circle, Tempe, AZ, USA')]");
-    }
-}
