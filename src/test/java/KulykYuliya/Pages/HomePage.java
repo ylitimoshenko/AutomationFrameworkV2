2ba7738701c5525bbca8ac3483d34262b05c55c7
Nestors <nestors@Yuliya-Kulyk-MacBook-Pro.local>
2022-04-18 13:38:02 +0300

    AddPagesPl4us1

--- src/test/java/KulykYuliya/Pages/HomePage.java
+++ src/test/java/KulykYuliya/Pages/HomePage.java
@@ -6,23 +6,19 @@ import pageobject.BasePage;
 
 public class HomePage extends BasePage {
 
-<<<<<<< HEAD
-=======
-    //public boolean IncorrectDataToLogIn;
 
->>>>>>> 59e34b76f2ef60c7a3f846f56202d3702c04874b
     public HomePage(WebDriver driver) {
         super(driver);
     }
 
-<<<<<<< HEAD
+
     public void OpenSiteEatStrt() {
         driver.get("https://qa2.eatstreet.com/");
     }
 
     public WebElement clickBtnSingIn() {
         return findElementByXpath("//a[@id='menu-signin']");
-=======
+
     public void OpenSiteEatStreet() {
         driver.get("https://qa2.eatstreet.com/");
     }
@@ -37,7 +33,6 @@ public class HomePage extends BasePage {
 
     public WebElement IncorrectDataToLogIn() {
         return findElementByXpath("//span[@ng-if='signinCtrl.loginInvalid']");
->>>>>>> 59e34b76f2ef60c7a3f846f56202d3702c04874b
 
     }
 }
