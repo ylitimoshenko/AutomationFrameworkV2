eeef7806bf52492d5d4e62505ed27157a961ddea
Bogdan27nazaruk <bodya.nazaruk409@gmail.com>
2022-04-18 22:07:23 +0300

    CheckSignUpError

--- src/test/java/NazarukBogdan/Tests/AboutEatStreetTests.java
+++ src/test/java/NazarukBogdan/Tests/AboutEatStreetTests.java
@@ -3,6 +3,7 @@ package NazarukBogdan.Tests;
 import NazarukBogdan.Pages.AboutUsPage;
 import NazarukBogdan.Pages.HomePage;
 import NazarukBogdan.Pages.SignInPage;
+import NazarukBogdan.Pages.SignUpPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import tests.TestInit;
@@ -41,6 +42,27 @@ public class AboutEatStreetTests extends TestInit {
         AboutUsPage aboutUsPage = new AboutUsPage(driver);
         sleep(2);
         Assert.assertTrue(aboutUsPage.getAboutUsInfo().isDisplayed());
+    }
+
+    @Test
+    public void SignUpError() {
+        HomePage homePage = new HomePage(driver);
+        openUrl("https://eatstreet.com/");
+        fullscreen();
+        homePage.getGotItBtn().click();
+        homePage.getSignInBtn().click();
+
+        SignInPage signInPage = new SignInPage(driver);
+        signInPage.getSignUpPageBtn().click();
+
+        SignUpPage signUpPage = new SignUpPage(driver);
+        signUpPage.getEmailField().sendKeys("QfdfskxcGmail.com");
+        signUpPage.getPassword().sendKeys("123wsx//745");
+        signUpPage.getPasswordAgain().sendKeys("123wsx//745");
+        signUpPage.getSignUpBtn().click();
+        sleep(2);
+        Assert.assertTrue(signUpPage.getErrorMSG().isDisplayed());
+
     }
 }
 
