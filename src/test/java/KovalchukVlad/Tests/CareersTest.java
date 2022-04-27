f40e701570f22034077c11bd7e43209ddb8282dd
KovalchukV1 <kovalchukvv001@gmail.com>
2022-04-27 18:50:44 +0300

    CareersTest

--- src/test/java/KovalchukVlad/Tests/CareersTest.java
+++ src/test/java/KovalchukVlad/Tests/CareersTest.java
@@ -1,2 +1,21 @@
-package KovalchukVlad.Tests;public class CareersTest {
+package KovalchukVlad.Tests;
+
+import KovalchukVlad.Pages.CareersPage;
+import KovalchukVlad.Pages.HomePage;
+import NazarukBogdan.Pages.DriverCareersPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import tests.TestInit;
+
+public class CareersTest extends TestInit {
+
+    @Test
+    public void checkButtonCareers(){
+        HomePage homePage = new HomePage(driver);
+        CareersPage careersPage = new CareersPage(driver);
+        homePage.openSiteEatStreet();
+        homePage.clickButtonGotIt();
+        homePage.clickButtonCareers();
+        Assert.assertTrue(careersPage.searchText().isDisplayed());
+    }
 }
