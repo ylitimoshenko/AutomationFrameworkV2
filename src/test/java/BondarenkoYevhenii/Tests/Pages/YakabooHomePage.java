3f922fc6952773dd62fa779ce5026717da1bac1e
Yevhenii <interdancecoach@gmail.com>
2022-06-27 15:45:49 +0300

    dayesh

--- src/test/java/BondarenkoYevhenii/Tests/Pages/YakabooHomePage.java
+++ src/test/java/BondarenkoYevhenii/Tests/Pages/YakabooHomePage.java
@@ -1,12 +1,8 @@
 package BondarenkoYevhenii.Tests.Pages;
 
 import ClasesToAll.BasePage;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-
-import java.util.List;
 
 public class YakabooHomePage extends BasePage {
     public YakabooHomePage(WebDriver driver) {
--- src/test/java/BondarenkoYevhenii/Tests/Tests/CheckIfColorOfTheButtonChangeToRed.java
+++ src/test/java/BondarenkoYevhenii/Tests/Tests/CheckIfColorOfTheButtonChangeToRed.java
@@ -2,6 +2,8 @@ package BondarenkoYevhenii.Tests.Tests;
 
 import BondarenkoYevhenii.Tests.Pages.YakabooHomePage;
 import ClasesToAll.TestInit;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -10,18 +12,20 @@ public class CheckIfColorOfTheButtonChangeToRed extends TestInit {
 
     @Test
 
-    public void checkIfColorOfButtonChangeToRed(){
+    public void checkIfColorOfButtonChangeToRed() {
         YakabooHomePage yakabooHomePage = new YakabooHomePage(driver);
         Actions action = new Actions(driver);
-
         openUrl("https://www.yakaboo.ua/");
         sleep(5);
+        JavascriptExecutor  js  = (JavascriptExecutor) driver;
+        String str = "return document.querySelector('body > div.grv-dialog-host').shadowRoot.querySelector('div > div > div.buttons-wrapper > button.sub-dialog-btn.block_btn')";
+        WebElement btn = (WebElement)((JavascriptExecutor)driver).executeScript(str);
+        btn.click();
         action.moveToElement(yakabooHomePage.aboutBtn()).perform();
-        Assert.assertEquals(yakabooHomePage.aboutBtn().getCssValue("color"),"rgba(234, 87, 61, 1)");
-          }
-
+        Assert.assertEquals(yakabooHomePage.aboutBtn().getCssValue("color"), "rgba(234, 87, 61, 1)");
     }
+}
 
 
-       // openUrl("https://www.yakaboo.ua/");
+
 
